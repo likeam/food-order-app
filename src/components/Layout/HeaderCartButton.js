07df7809075a0558d91ea494328fa7891e90afd4
Abdul Rehamn <likeam99@gmail.com>
2024-01-11 21:43:11 +0500

    Building Cart

---
+++ src/components/Layout/HeaderCartButton.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import CartIcon from './CartIcon';
+import classes from './HeaderCartButton.module.css'
+
+const HeaderCartButton = props => {
+  return (
+    <button className={classes.button}>
+        <span className={classes.icon}>
+            <CartIcon />
+        </span>
+        <span className={classes.badge} >Your Cart</span>
+        <span>3</span>
+    </button>
+  )
+}
+
+export default HeaderCartButton;
---
+++ src/components/Meals/MealsItems/MealsItemForm.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import classes from './MealsItemForm.module.css'
+
+const MealsItemForm = () => {
+  return (
+    <form className={classes.form}>
+        <input />
+        <button>+ Add</button>
+    </form>
+  )
+}
+
+export default MealsItemForm;
